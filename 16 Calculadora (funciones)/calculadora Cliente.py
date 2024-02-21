1e71acb138d84d72ed55de95775f201c286f3f79
egallofre80 <informatica2@reus.lasalle.cat>
2024-02-21 08:48:14 +0100

    Modificado el control de errores  en calculadora cliente

--- 16 Calculadora (funciones)/calculadora Cliente.py
+++ 16 Calculadora (funciones)/calculadora Cliente.py
@@ -3,11 +3,12 @@ import calculadora
 try :
     operador1 = int(input("Introdueix el primer operador: "))
     operador2 = int(input("Introdueix el segon operador: "))
+
+    calculadora.suma(operador1, operador2)
+    calculadora.resta(operador1, operador2)
+    calculadora.mult(operador1, operador2)
+    calculadora.division(operador1, operador2)
+    calculadora.exponencial(operador1,  operador2)
 except ValueError:
         print("Número no válida")
 
-calculadora.suma(operador1, operador2)
-calculadora.resta(operador1, operador2)
-calculadora.mult(operador1, operador2)
-calculadora.division(operador1, operador2)
-calculadora.exponencial(operador1,  operador2)
